9bf00e8fdb5f6ef40bad8862462af23a1c9d0090
Mhd Ulil Abshar <87646428+howlil@users.noreply.github.com>
2026-09-05 05:32:54 +0700

    feat(sop): add workflow projection to workbench dto

--- server/src/modules/sop/catalog/dto/penyusun-workbench-data.dto.ts
+++ server/src/modules/sop/catalog/dto/penyusun-workbench-data.dto.ts
@@ -4,6 +4,7 @@ import { PenyusunWorkbenchLangkahDto } from './penyusun-workbench-langkah.dto';
 import { PenyusunWorkbenchLogEditDto } from './penyusun-workbench-log-edit.dto';
 import { PenyusunWorkbenchDiagramKonfigurasiDto } from '../../diagram/dto/penyusun-workbench-diagram.dto';
 import { BeritaAcaraTteSignaturePayloadDto } from '../../../evaluation/pengajuan-detail/dto/berita-acara-evaluasi-view.dto';
+import { SopWorkflowProjectionDto } from './sop-workflow.dto';
 
 /** Muatan data GET area kerja penyusun: detail + semua langkah + log (satu respons). */
 export class PenyusunWorkbenchDataDto {
@@ -24,4 +25,7 @@ export class PenyusunWorkbenchDataDto {
 
   @ApiProperty({ type: () => BeritaAcaraTteSignaturePayloadDto, required: false })
   readonly tteSignaturePayloadKepalaOpd?: BeritaAcaraTteSignaturePayloadDto;
+
+  @ApiProperty({ type: () => SopWorkflowProjectionDto, required: false })
+  readonly workflow?: SopWorkflowProjectionDto;
 }
