abbd243e09184f853941b59897b4a456915b293a
Mhd Ulil Abshar <87646428+howlil@users.noreply.github.com>
2026-09-05 10:23:56 +0700

    refactor(tte): bind internal P12 signing provider

--- server/src/modules/tte/penandatanganan/tte-penandatanganan.module.ts
+++ server/src/modules/tte/penandatanganan/tte-penandatanganan.module.ts
@@ -3,10 +3,20 @@ import { SopPdfModule } from '../../sop/pdf/sop-pdf.module';
 import { TteSharedModule } from '../shared/tte-shared.module';
 import { TtePenandatangananService } from './tte-penandatanganan.service';
 import { TtePdfSigningService } from './tte-pdf-signing.service';
+import { DOCUMENT_SIGNING_PROVIDER } from './signing/document-signing.provider';
+import { InternalP12SigningProvider } from './signing/internal-p12-signing.provider';
 
 @Module({
   imports: [TteSharedModule, SopPdfModule],
-  providers: [TtePenandatangananService, TtePdfSigningService],
+  providers: [
+    TtePenandatangananService,
+    TtePdfSigningService,
+    InternalP12SigningProvider,
+    {
+      provide: DOCUMENT_SIGNING_PROVIDER,
+      useExisting: InternalP12SigningProvider,
+    },
+  ],
   exports: [TtePenandatangananService, TtePdfSigningService],
 })
 export class TtePenandatangananModule {}
