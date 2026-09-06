05c5247935bd8e4978fbef2a880912a4c6bdea11
Mhd Ulil Abshar <87646428+howlil@users.noreply.github.com>
2026-09-06 18:32:00 +0700

    M31: use existing PJ Evaluator index for work queue

--- client/src/routes/pj-evaluator/pekerjaan/index.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { WorkItemsPage } from '@/pages/work-items/WorkItemsPage'
-
-export const Route = createFileRoute('/pj-evaluator/pekerjaan/')({
-  component: WorkItemsPage,
-})
