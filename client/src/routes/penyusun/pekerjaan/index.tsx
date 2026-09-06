be94f85e250c96554258242aaa66c04f1a23669a
Mhd Ulil Abshar <87646428+howlil@users.noreply.github.com>
2026-09-06 18:32:17 +0700

    M31: use existing Penyusun index for work queue

--- client/src/routes/penyusun/pekerjaan/index.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { WorkItemsPage } from '@/pages/work-items/WorkItemsPage'
-
-export const Route = createFileRoute('/penyusun/pekerjaan/')({
-  component: WorkItemsPage,
-})
