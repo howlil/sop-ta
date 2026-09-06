86a43ae340946c184648af759fa1d82bfef33c2f
Mhd Ulil Abshar <87646428+howlil@users.noreply.github.com>
2026-09-06 18:26:55 +0700

    M31: route Kepala OPD to work queue

---
+++ client/src/routes/kepala-opd/pekerjaan/index.tsx
@@ -0,0 +1,6 @@
+import { createFileRoute } from '@tanstack/react-router'
+import { WorkItemsPage } from '@/pages/work-items/WorkItemsPage'
+
+export const Route = createFileRoute('/kepala-opd/pekerjaan/')({
+  component: WorkItemsPage,
+})
