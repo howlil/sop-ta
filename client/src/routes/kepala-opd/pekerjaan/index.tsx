ed3abde9a6232e144cdce3e585b19fc3bbb2fe34
Mhd Ulil Abshar <87646428+howlil@users.noreply.github.com>
2026-09-06 18:32:23 +0700

    M31: use existing Kepala OPD index for work queue

--- client/src/routes/kepala-opd/pekerjaan/index.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { WorkItemsPage } from '@/pages/work-items/WorkItemsPage'
-
-export const Route = createFileRoute('/kepala-opd/pekerjaan/')({
-  component: WorkItemsPage,
-})
