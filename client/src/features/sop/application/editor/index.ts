49b0ae8b3f95b41b8d91c3a7a70542c01bea1ee6
Mhd Ulil Abshar <87646428+howlil@users.noreply.github.com>
2026-09-16 12:04:59 +0700

    refactor(client): expose SOP editor application facade

---
+++ client/src/features/sop/application/editor/index.ts
@@ -0,0 +1,28 @@
+export {
+  useDetailSopPenyusun,
+  useDetailSopPenyusunActions,
+  useDetailSopPenyusunData,
+  type UseDetailSopPenyusunActionsParams,
+  type UseDetailSopPenyusunDataResult,
+  type UseDetailSopPenyusunReturn,
+} from './use-detail-sop-penyusun'
+
+export {
+  buildSopHeaderSnapshot,
+  diffSopHeaderSnapshots,
+  useSopHeaderAutosave,
+  type SopHeaderAutosaveControls,
+  type SopHeaderAutosaveStatus,
+  type SopHeaderSnapshot,
+  type UseSopHeaderAutosaveOptions,
+} from './use-sop-header-autosave'
+
+export {
+  buildSopProsedurSnapshot,
+  diffSopProsedurSnapshots,
+  useSopProsedurAutosave,
+  type SopProsedurAutosaveControls,
+  type SopProsedurAutosaveStatus,
+  type SopProsedurSnapshot,
+  type UseSopProsedurAutosaveOptions,
+} from './use-sop-prosedur-autosave'
